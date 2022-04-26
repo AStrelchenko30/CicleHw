2841aeb8de60187053914588c5cec0ff2a190107
VIKI <anystrelchenko30@gmail.com>
2022-04-26 14:27:41 +0300

    ОСновная домашняя работа по циклам

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,37 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("привет");
-    }
-}
+        //Задача 1
+        int a = 1;
+        while (a <= 10) {
+            if (a == 10) {
+                System.out.println(a);
+                break;
+            }
+            System.out.print(a + " ");
+            a++;
+        }
+        for (a = 10; a >= 1; a--) {
+            System.out.print(a + " ");
+        }
+        System.out.println(" ");
+//задача 2
+        for (int firstFriday = 4; firstFriday <= 31; firstFriday += 7) {
+            System.out.println("Сегодня пятница " + firstFriday + "-е числа.Необходимо подготовить отчет");
+        }
+        // Задача 3
+        int i=0;
+        int ourYear=2022;
+        int oldYear=ourYear-200;
+        int newYear=ourYear+100;
+        while (i<ourYear){
+            i+=76;
+            }
+        int lastFall=i-(76*2);
+        while (lastFall<=newYear){
+            System.out.println(lastFall);
+            lastFall+=76;
+        }
+        }
+        }
+
